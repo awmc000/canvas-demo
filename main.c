c067eab38d6e39e69b83095ba3410f4c47c2b864
Alex McColm <mccolmalex@gmail.com>
2025-07-17 12:09:17 -0700

    Extract functions from gameLoop; sketch out structs

--- main.c
+++ main.c
@@ -25,17 +25,44 @@
 #define BORDER_MARGIN 10
 
 /*****************************************************************************
- * Global Variables
+ * Structs and Typedefs
  *****************************************************************************/
-RenderTexture rt;
-struct {
+
+struct viewport {
     int y;
     int x;
     int w;
     int h;
-} viewport;
+};
+
+struct object {
+
+    // sticky = 1: Should be drawn at edge when offscreen. 
+    // sticky = 0: Hidden when offscreen.
+    int sticky: 1;
+
+    // A label that can be drawn next to this object
+    char * label;
+
+    // TODO: Generalize to more than circles!
+    // Circle radius.
+    int radius;
+
+    // TODO: Implement textures, etc.
+    Color color;
+
+    // Object world position with origin at top left
+    int y, x;
+};
+
+/*****************************************************************************
+ * Global Variables
+ *****************************************************************************/
+RenderTexture rt;
+struct viewport vp;
 
 const char* someResource = 0;
+Vector2 mouseDragPoint;
 
 #ifdef __EMSCRIPTEN__
     EM_JS(void, idbfs_put, (const char* filename, const char* str), {
@@ -60,16 +87,16 @@ const char* someResource = 0;
 */
 int clampProjectY(int positionY) {
     // Case: positionY is above the screen => clamp to top border
-    if (positionY < viewport.y) {
+    if (positionY < vp.y) {
         return GetScreenHeight() - BORDER_MARGIN;
     }
     // Case: positionY is below the screen => clamp to bottom border
-    else if (positionY > viewport.y + viewport.h) {
+    else if (positionY > vp.y + vp.h) {
         return BORDER_MARGIN;
     }
     // Case: positionY is on screen
     else {
-        return GetScreenHeight() - (positionY - viewport.y);
+        return GetScreenHeight() - (positionY - vp.y);
     }
 }
 
@@ -78,31 +105,27 @@ int clampProjectY(int positionY) {
 */
 int clampProjectX(int positionX) {
     // Case: positionX is left of the screen => clamp to left border
-    if (positionX < viewport.x) {
+    if (positionX < vp.x) {
         return BORDER_MARGIN;
     }
     // Case: positionX is right of the screen => clamp to right border
-    else if (positionX > viewport.x + viewport.w) {
-        return viewport.x + viewport.w - BORDER_MARGIN;
+    else if (positionX > vp.x + vp.w) {
+        return vp.x + vp.w - BORDER_MARGIN;
     }
     // Case: positionX is on screen
     else {
-        return positionX - viewport.x;
+        return positionX - vp.x;
     }
 }
 
-void gameLoop() {
+void handleInput() {
+    if (IsKeyDown(KEY_W) && vp.y > 0) vp.y--;
+    if (IsKeyDown(KEY_S)) vp.y++;
+    if (IsKeyDown(KEY_A) && vp.x > 0) vp.x--;
+    if (IsKeyDown(KEY_D)) vp.x++;
+}
 
-    // Input handling
-    if (IsKeyDown(KEY_W) && viewport.y > 0) viewport.y--;
-    if (IsKeyDown(KEY_S)) viewport.y++;
-    if (IsKeyDown(KEY_A) && viewport.x > 0) viewport.x--;
-    if (IsKeyDown(KEY_D)) viewport.x++;
-
-    // Drawing to render texture
-    BeginDrawing();
-    BeginTextureMode(rt);
-    ClearBackground(CLITERAL(Color){255, 255, 255, 255});
+void drawObjects() {
     // DrawCircle(
     //     GetMouseX(), 
     //     GetScreenHeight() - GetMouseY(), 
@@ -115,12 +138,30 @@ void gameLoop() {
         10, 
         CLITERAL(Color){10,10,200,255}
     );
+    DrawLine(
+        clampProjectX(50), 
+        clampProjectY(100),
+        clampProjectX(1000), 
+        clampProjectY(1000),
+        CLITERAL(Color){20,10,10,255}
+    );
     DrawCircle(
         clampProjectX(1000), 
         clampProjectY(1000), 
         10, 
         CLITERAL(Color){200,10,20,255}
     );
+}
+
+void gameLoop() {
+
+    handleInput();
+
+    // Drawing to render texture
+    BeginDrawing();
+    BeginTextureMode(rt);
+    ClearBackground(CLITERAL(Color){255, 255, 255, 255});
+    drawObjects();
     EndTextureMode();
     DrawTexture(rt.texture, 0, 0, CLITERAL(Color){255,255,255,255});
     DrawFPS(0, 0);
@@ -129,7 +170,7 @@ void gameLoop() {
         DrawText(TextFormat("Dynamic file content: %s", text), 0, 30, 20, WHITE);
     #endif
     DrawText(TextFormat("Static file content: %s", someResource), 0, 60, 20, BLACK);
-    DrawText(TextFormat("(%d, %d)", viewport.y, viewport.x), 100, 0, 20, BLACK);
+    DrawText(TextFormat("(%d, %d)", vp.y, vp.x), 100, 0, 20, BLACK);
     EndDrawing();
 }
 
@@ -137,10 +178,10 @@ int main() {
     int screenWidth = 960;
     int screenHeight = 480;
 
-    viewport.y = 500;
-    viewport.x = 500;
-    viewport.w = screenWidth;
-    viewport.h = screenHeight;
+    vp.y = 500;
+    vp.x = 500;
+    vp.w = screenWidth;
+    vp.h = screenHeight;
 
     InitWindow(screenWidth, screenHeight, "rtextures");
     SetTargetFPS(144);
