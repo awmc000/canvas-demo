440a1b6b46953c4702fb4b9078db2087ded3396b
Alex McColm <mccolmalex@gmail.com>
2025-07-17 18:51:34 -0700

    Crude system for drawing dots or sprites

--- main.c
+++ main.c
@@ -24,6 +24,7 @@
 #define LOG(X) printf("%s: %d", __FILE__, __LINE__, X)
 #define BORDER_MARGIN 10
 #define MOUSE_ACTIVE_CLOCK_TICKS 15
+#define MAX_OBJECTS 256
 
 /*****************************************************************************
  * Structs and Typedefs
@@ -36,14 +37,22 @@ struct viewport {
     int h;
 };
 
+enum objectType {
+    DOT,
+    LINE,
+    SPRITE
+};
+
 struct object {
+
+    enum objectType type;
 
     // sticky = 1: Should be drawn at edge when offscreen. 
     // sticky = 0: Hidden when offscreen.
     int sticky: 1;
 
     // A label that can be drawn next to this object
-    char * label;
+    const char * label;
 
     // TODO: Generalize to more than circles!
     // Circle radius.
@@ -60,16 +69,21 @@ struct object {
  * Global Variables
  *****************************************************************************/
 RenderTexture rt;
+Texture2D skull;
 struct viewport vp;
 
 const char* someResource = 0;
 
-// Mouse drag stuff
+// The point clicked to drag the viewport.
 Vector2 mouseDragPoint;
-Vector2 mouseOffset;
+Vector2 dragViewportFrom;
 clock_t lastMouseActivity;
 clock_t currentTime;
 int mouseMoving = 0;
+
+// Objects array
+struct object objs[MAX_OBJECTS];
+int objsLen = 0;
 
 #ifdef __EMSCRIPTEN__
     EM_JS(void, idbfs_put, (const char* filename, const char* str), {
@@ -90,16 +104,25 @@ int mouseMoving = 0;
 #endif
 
 /**
+ * Adds an object to the object array and returns its index.
+*/
+int createObject(struct object newObj) {
+    objs[objsLen] = newObj;
+    objsLen++;
+    return objsLen - 1;
+}
+
+/**
  * Converts a Position Y coordinate to a screen Y coordinate.
 */
-int clampProjectY(int positionY) {
+int clampProjectY(int positionY, int clamp) {
     // Case: positionY is above the screen => clamp to top border
     if (positionY < vp.y) {
-        return GetScreenHeight() - BORDER_MARGIN;
+        return clamp ? GetScreenHeight() - BORDER_MARGIN : -1000;
     }
     // Case: positionY is below the screen => clamp to bottom border
     else if (positionY > vp.y + vp.h) {
-        return BORDER_MARGIN;
+        return clamp ? BORDER_MARGIN : -1000;
     }
     // Case: positionY is on screen
     else {
@@ -110,14 +133,14 @@ int clampProjectY(int positionY) {
 /**
  * Converts a Position X coordinate to a screen X coordinate.
 */
-int clampProjectX(int positionX) {
+int clampProjectX(int positionX, int clamp) {
     // Case: positionX is left of the screen => clamp to left border
     if (positionX < vp.x) {
-        return BORDER_MARGIN;
+        return clamp ? BORDER_MARGIN : -1000;
     }
     // Case: positionX is right of the screen => clamp to right border
     else if (positionX > vp.x + vp.w) {
-        return vp.x + vp.w - BORDER_MARGIN;
+        return clamp ? vp.x + vp.w - BORDER_MARGIN : -1000;
     }
     // Case: positionX is on screen
     else {
@@ -139,36 +162,64 @@ void handleInput() {
     // Move viewport relative to drag point while LMB is down and moving
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         mouseDragPoint = GetMousePosition();
-        mouseOffset.y = vp.y;
-        mouseOffset.x = vp.x;
+        dragViewportFrom.y = vp.y;
+        dragViewportFrom.x = vp.x;
     }
     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
         lastMouseActivity = clock() / (1000);
-        vp.x = (mouseOffset.x - GetMouseX() + mouseDragPoint.x);
-        vp.y = (mouseOffset.y - GetMouseY() + mouseDragPoint.y);
+        vp.x = (dragViewportFrom.x - GetMouseX() + mouseDragPoint.x);
+        vp.y = (dragViewportFrom.y - GetMouseY() + mouseDragPoint.y);
     }
 }
 
 void drawObjects() {
-    DrawCircle(
-        clampProjectX(50), 
-        clampProjectY(100), 
-        10, 
-        CLITERAL(Color){10,10,200,255}
-    );
-    DrawLine(
-        clampProjectX(50), 
-        clampProjectY(100),
-        clampProjectX(1000), 
-        clampProjectY(1000),
-        CLITERAL(Color){20,10,10,255}
-    );
-    DrawCircle(
-        clampProjectX(1000), 
-        clampProjectY(1000), 
-        10, 
-        CLITERAL(Color){200,10,20,255}
-    );
+    struct object curr;
+
+    for (int i = 0; i < objsLen; i++) {
+        curr = objs[i];
+        switch (curr.type) {
+            case DOT:
+                DrawCircle(
+                    clampProjectX(curr.x, curr.sticky), 
+                    clampProjectY(curr.y, curr.sticky), 
+                    curr.radius, 
+                    curr.color
+                );
+                break;
+            case SPRITE:
+                // void DrawTextureEx(Texture2D texture, Vector2 position, float rotation, float scale, Color tint);
+                Vector2 pos;
+                pos.x = clampProjectX(curr.x, curr.sticky); 
+                pos.y = clampProjectY(curr.y, curr.sticky); 
+                DrawTextureEx(
+                    skull,
+                    pos,
+                    180.0,
+                    1.0,
+                    curr.color
+                );
+                break;
+        }
+
+    }
+
+    
+}
+
+void drawLabels() {
+    struct object curr;
+
+    for (int i = 0; i < objsLen; i++) {
+        curr = objs[i];
+        DrawText(
+            curr.label,
+            clampProjectX(curr.x, curr.sticky) + curr.radius + 5, 
+            vp.h - (clampProjectY(curr.y, curr.sticky) + curr.radius + 5),
+            20,
+            BLACK
+        );
+    }
+
 }
 
 void gameLoop() {
@@ -190,10 +241,10 @@ void gameLoop() {
         char* text = idbfs_get("file.txt");
         DrawText(TextFormat("Dynamic file content: %s", text), 0, 30, 20, WHITE);
     #endif
-    DrawText(TextFormat("Static file content: %s", someResource), 0, 60, 20, BLACK);
-    DrawText(TextFormat("(%d, %d)", vp.y, vp.x), 100, 0, 20, BLACK);
-    DrawText(TextFormat("(%f, %f)", mouseDragPoint.y, mouseDragPoint.x), 100, 100, 20, BLACK);
-    DrawText(TextFormat("lma: %d curr: %d", lastMouseActivity, currentTime), 100, 120, 20, BLACK);
+    DrawText(TextFormat("viewport: (%d, %d)", vp.y, vp.x), 0, 20, 20, BLACK);
+    DrawText(TextFormat("drag pt: (%f, %f)", mouseDragPoint.y, mouseDragPoint.x), 0, 40, 20, BLACK);
+    DrawText(TextFormat("mouse last moving: %d curr time: %d", lastMouseActivity, currentTime), 0, 60, 20, BLACK);
+    drawLabels();
     if (mouseMoving)
         DrawText("Mouse moving", 300, 20, 20, BLUE);
     EndDrawing();
@@ -212,6 +263,39 @@ int main() {
     SetTargetFPS(144);
     rt = LoadRenderTexture(screenWidth, screenHeight);
     someResource = LoadFileText("resources/dummy.txt");
+    skull = LoadTexture("resources/skull-wenrexa.png");
+
+    struct object dot = {
+        .type = DOT,
+        .sticky = 0,
+        .label="A dot",
+        .radius = 10,
+        .color = BLUE,
+        .y = 100,
+        .x = 100,
+    }; 
+    createObject(dot);
+
+    struct object dot2 = {
+        .type = DOT,
+        .sticky = 1,
+        .label="Dot 2",
+        .radius = 15,
+        .color = RED,
+        .y = 150,
+        .x = 90,
+    }; 
+    createObject(dot2);
+
+    struct object skullObj = {
+        .type = SPRITE,
+        .sticky = 0,
+        .label = "EVIL SKULL",
+        .color = BLACK,
+        .y = 200,
+        .x = 300
+    };
+    createObject(skullObj);
 
     #ifdef __EMSCRIPTEN__
         EM_ASM(
