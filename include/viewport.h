2f6b89197bbeb7f252358a421150c809ad6b46d0
Alex McColm <mccolmalex@gmail.com>
2025-07-24 10:17:32 -0700

    Format headers

--- include/viewport.h
+++ include/viewport.h
@@ -1,43 +1,46 @@
 /**
  * The plane is generally larger than the screen, and the viewport is the rectangle
  * currently being viewed in the window.
-*/
+ */
 #ifndef VIEWPORT_H
 #define VIEWPORT_H
 
 #include "raylib.h"
-#define BORDER_MARGIN               10
+#define BORDER_MARGIN 10
 
 /*****************************************************************************
- * Structs and Typedefs
- *****************************************************************************/
+* Structs and Typedefs
+*****************************************************************************/
 
 struct viewport {
-    int y;
-    int x;
-    int w;
-    int h;
+    int   y;
+    int   x;
+    int   w;
+    int   h;
     float scale; // TODO: implement zoom, which will require scaling all drawn objects
 };
 
 /*****************************************************************************
- * Functions Provided
- *****************************************************************************/
+* Functions Provided
+*****************************************************************************/
 
 /**
  * Converts a Position Y coordinate to a screen Y coordinate.
-*/
-int clampProjectX(struct viewport * vp, int positionX, int clamp);
+ */
+int
+clampProjectX(struct viewport * vp, int positionX, int clamp);
 
 /**
  * Converts a Position X coordinate to a screen X coordinate.
-*/
-int clampProjectY(struct viewport * vp, int positionX, int clamp);
+ */
+int
+clampProjectY(struct viewport * vp, int positionX, int clamp);
 
 /**
  * Returns 1 if the given world position is visible
- * in the current viewport. 
-*/
-int positionVisible(struct viewport * vp, int y, int x);
+ * in the current viewport.
+ */
+int
+positionVisible(struct viewport * vp, int y, int x);
 
 #endif // VIEWPORT_H
